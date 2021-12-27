219d74d4fdc8c82e1ae96a92365600496031c937
Meiling Bedard <meiling.bedard@gmail.com>
2021-12-26 20:19:20 -0500

    add fish index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Fish, Decoration
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,7 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def fish_index(request):
+  fish = Fish.objects
+  return render(request, 'fish/index.html', { 'fish': fish })
