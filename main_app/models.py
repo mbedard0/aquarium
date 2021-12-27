b8c76615a12827097ef349b2e6fbca707b23fb42
Meiling Bedard <meiling.bedard@gmail.com>
2021-12-26 19:51:15 -0500

    add models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Fish(models.Model):
+  name = models.CharField(max_length=40)
+  species = models.CharField(max_length=40)
+  color = models.CharField(max_length=20)
+  price = models.IntegerField
+  age = models.IntegerField
+  decorations = models.ManyToManyField(Decoration)
+
+class Decoration(models.Model):
+  name = models.CharField(max_length=40)
+  color = models.CharField(max_length=20)
+  description = models.CharField(max_length=300)
+  price = models.IntegerField
