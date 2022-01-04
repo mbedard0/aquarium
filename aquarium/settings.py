63de0673c1c4bbe8a807070dc887ac1e20e3f8ce
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-04 13:21:47 -0500

    add alternativedatabase

--- aquarium/settings.py
+++ aquarium/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': 'aquarium',
+# 	}
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
